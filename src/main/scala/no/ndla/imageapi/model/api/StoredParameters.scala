9ba3f3c581d4cf94ff117cdb5c9cce1f623f029c
Runar Furenes <ruf@knowit.no>
2018-05-24 14:48:58 +0200

    #348: Description of the StoredParameters types GlobalDigitalLibraryio/issues#348

--- src/main/scala/no/ndla/imageapi/model/api/StoredParameters.scala
+++ src/main/scala/no/ndla/imageapi/model/api/StoredParameters.scala
@@ -4,20 +4,19 @@ import org.scalatra.swagger.annotations.{ApiModel, ApiModelProperty}
 
 import scala.annotation.meta.field
 
-// TODO Fyll ut
-@ApiModel(description = "")
-case class StoredParameters(@(ApiModelProperty@field)(description = "") imageUrl: String,
-                            @(ApiModelProperty@field)(description = "") forRatio: String,
-                            @(ApiModelProperty@field)(description = "") revision: Option[Int],
-                            @(ApiModelProperty@field)(description = "") rawImageQueryParameters: RawImageQueryParameters)
+@ApiModel(description = "Information about stored parameters for an image")
+case class StoredParameters(@(ApiModelProperty@field)(description = "The URL of an image, as it's stored in the metadata") imageUrl: String,
+                            @(ApiModelProperty@field)(description = "This, together with the imageUrl, is the lookup key") forRatio: String,
+                            @(ApiModelProperty@field)(description = "Revision in the database") revision: Option[Int],
+                            @(ApiModelProperty@field)(description = "The actual parameters stored for an image and a ratio") rawImageQueryParameters: RawImageQueryParameters)
 
-@ApiModel(description = "")
-case class RawImageQueryParameters(@(ApiModelProperty@field)(description = "") width: Option[Int],
-                                   @(ApiModelProperty@field)(description = "") height: Option[Int],
-                                   @(ApiModelProperty@field)(description = "") cropStartX: Option[Int],
-                                   @(ApiModelProperty@field)(description = "") cropStartY: Option[Int],
-                                   @(ApiModelProperty@field)(description = "") cropEndX: Option[Int],
-                                   @(ApiModelProperty@field)(description = "") cropEndY: Option[Int],
-                                   @(ApiModelProperty@field)(description = "") focalX: Option[Int],
-                                   @(ApiModelProperty@field)(description = "") focalY: Option[Int],
-                                   @(ApiModelProperty@field)(description = "") ratio: Option[String])
+@ApiModel(description = "Parameters used to crop and resize an image through the raw-endpoint")
+case class RawImageQueryParameters(@(ApiModelProperty@field)(description = "Width of the image in pixels") width: Option[Int],
+                                   @(ApiModelProperty@field)(description = "Height of the image in pixels") height: Option[Int],
+                                   @(ApiModelProperty@field)(description = "Where to start cropping from the left side, in percent") cropStartX: Option[Int],
+                                   @(ApiModelProperty@field)(description = "Where to start cropping from the top, in percent") cropStartY: Option[Int],
+                                   @(ApiModelProperty@field)(description = "Where to stop cropping from the left side, in percent") cropEndX: Option[Int],
+                                   @(ApiModelProperty@field)(description = "Where to end cropping from the top, in percent") cropEndY: Option[Int],
+                                   @(ApiModelProperty@field)(description = "Where a focal point is located from the left side, in percent") focalX: Option[Int],
+                                   @(ApiModelProperty@field)(description = "Where a focal point is located from the top, in percent") focalY: Option[Int],
+                                   @(ApiModelProperty@field)(description = "Wanted ratio for the image") ratio: Option[String])
